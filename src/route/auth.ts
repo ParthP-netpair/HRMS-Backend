eda4110717aa1b2f69e49995bef75f8d2dd223ba
parth-alineah <parth.parmar@alineahealthcare.in>
2025-06-25 10:57:15 +0530

    git

--- src/route/auth.ts
+++ src/route/auth.ts
@@ -10,7 +10,6 @@ import { AUTH_ROUTE } from '../utils/route.enums';
 import authMw from '../middleware/auth';
 
 const authRoute = express.Router();
-
 authRoute.post(AUTH_ROUTE.signup, signupCtl);
 authRoute.post(AUTH_ROUTE.login, loginCtl);
 authRoute.post(AUTH_ROUTE.forgotPassword, forgotPasswordCtl);
